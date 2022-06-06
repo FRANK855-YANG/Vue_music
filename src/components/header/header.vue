a66406cfc8bea185802916e88afce0046ed07d8c
frank855 <zhenyue.yang@outlook.com>
2022-06-06 21:27:32 +0800

    set up

---
+++ src/components/header/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+      <span class="icon"></span>
+      <h1 class="text">Frank Music</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'MyHeader'
+}
+</script>
+
+<style>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        //全局引入variable和mixin
+        additionalData:
+          '@import "@/assets/scss/variable.scss";@import "@/assets/scss/mixin.scss"'
+      }
+    }
+  }
 })
